fcb0bf132a66d3b5c36709672b4dce5b701cee4f
lodhaakash07 <lodhaakash07@gmail.com>
2014-09-06 23:54:10 +0530

    Plot1 R script and image

---
+++ plot1.R
@@ -0,0 +1,19 @@
+# Akash Lodha
+# Data Science Specialisation Coursera
+# Exploratory Data Analysis - Peer Assignment 1
+#[1] "Sat Sep  6 12:05:18 2014"
+
+## plot1.R
+
+##Getting and Processing the data
+##Creating a final data set for this peer assignment has been done in
+## dataProcess.R R script
+source("./dataProcess.R") # This has all the code for getting the data needed
+## The required data is saved in data.txt
+
+# Read the data
+data <- read.csv("./data.csv")
+with(data, hist(Global_active_power,col="Red",xlab="Global Active Power (kilowatts)",ylab="Frequency",main="Global Active Power"))
+dev.copy(png,"./plot1.png",height=480,width=480)
+dev.off()
+data<-NULL
